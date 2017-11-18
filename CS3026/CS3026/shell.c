cb20e7a1f7af4df0e9a2b67d515c060b0eafc23f
Pattio <merfisku@gmail.com>
2017-11-18 12:49:02 +0000

    Init repository

---
+++ CS3026/CS3026/shell.c
@@ -0,0 +1,15 @@
+//
+//  shell.c
+//  CS3026
+//
+//  Created by Edvinas on 18/11/2017.
+//  Copyright © 2017 Edvinas. All rights reserved.
+//
+
+#include <stdio.h>
+#include "filesys.h"
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
